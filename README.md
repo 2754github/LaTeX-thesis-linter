75410b032c3f32987d7eb3efa0153339a9f6f192
2754github <fkesys@gmail.com>
2020-11-17 16:28:41 +0900

    First commit🎉

---
+++ README.md
@@ -0,0 +1,64 @@
+# 概要
+
+`LaTeX`で執筆された日本語論文の校正を行うツールです。
+
+# 動作に必要なもの
+
+- `Node.js`: `8.2.0`以上
+- `npm`: `5.2.0`以上
+- `Ruby`: `2.6.0`以上
+
+# セットアップ
+
+```zsh:ターミナル
+$ git clone https://github.com/2754github/LaTeX-thesis-linter.git # DL&解答でも可
+$ cd <LaTeX-thesis-linterフォルダへのpath>
+$ npm install
+```
+
+# 使用方法
+
+```zsh:ターミナル
+$ cd <LaTeX-thesis-linterフォルダへのpath>
+$ ruby file_linter.rb <校正したいファイルへのpath>
+
+# サンプル
+$ ruby file_linter.rb sample/bad_sentences.tex
+```
+
+# カスタマイズ
+
+- `.textlintrc`: `textlint`の各ルールを設定します
+- `prh.yml`: 表記の統一ルールを設定します
+  - [`prh.yml`の書き方](https://github.com/prh/prh/blob/master/misc/prh.yml)
+  - [このリスト](https://raw.githubusercontent.com/WorksApplications/SudachiDict/develop/src/main/text/synonyms.txt)にあるものは`@textlint-ja/no-synonyms`のルールに内包されているので`prh.yml`には書かなくて大丈夫です
+- `file_linter.rb`:
+  - このファイルから`textlint`を呼び出してファイルを走査します
+  - さらに`textlint`で対応できない追加のルール（カンマ/ピリオドに関するルール等）を付与してファイルを走査します
+
+<details>
+<summary>「`textlint`で対応できない追加のルール」はこちら</summary>
+
+- 全角のスペース/読点/句点/カンマ/ピリオド/丸かっこを使用しない
+- 半角のスペース/カンマ/ピリオド/丸かっこ（以下「␣」「,」「.」「(」「)」）を使用する
+- 「,」「.」の前に「␣」を挿入しない
+- 「,」「.」の後に「␣」を挿入する
+- 「)」の前に「␣」を挿入しない
+- 「(」の前に「␣」を挿入する
+- 「,␣(改行)」「,(改行)」で文が終了している場合に警告する
+- 文頭が数式の場合に警告する
+- ファイルの最後には改行を挿入する
+
+</details>
+
+# それでも対応できないルールは・・・
+
+「`grep`をかける」「pdf を検索する」など、地道に確認しましょう・・・😭
+
+# 参考
+
+- [textlint-ja](https://github.com/textlint-ja): 文章校正ツール（`LaTeX`非対応 😭）
+- [textlint-plugin-latex2e](https://github.com/textlint/textlint-plugin-latex2e): `textlint-ja`で`LaTeX`ファイルが読み込めるようになるプラグイン（神 ☺️）
+- [LaTeX における正しい論文の書き方](https://qiita.com/birdwatcher/items/5ec42b35d84d3ee2ffbb)
+
+#### 「ご意見」「ルール追加/削除のご要望」「バグ報告」などありましたら、お気軽にお寄せください 🙇‍♂️
---
+++ file_linter.rb
@@ -0,0 +1,99 @@
+def file_linter(file_name)
+  system("npx textlint #{file_name}")
+  warning_count = 0
+  error_count = 0
+  File.open(file_name, "r:UTF-8") do |file|
+    file.each_with_index do |line, i|
+      tmp = comma_and_period_rule(line, i)
+      warning_count += tmp[0]
+      error_count += tmp[1]
+      warning_count += formula_rule(line, i)
+      error_count += posix_rule(line, i)
+    end
+  end
+  puts "\n\e[1m\e[31m✖ and #{error_count+warning_count} problems (#{error_count} errors, #{warning_count} warnings)\e[m\e[m"
+end
+
+def comma_and_period_rule(line, i)
+  warning_count = 0
+  error_count = 0
+
+  replace_words = {
+    "　" => "␣",
+    "、" => ",␣",
+    "。" => ".␣",
+    "，" => ",␣",
+    "．" => ".␣",
+    "（" => "␣(",
+    "）" => ")␣"
+    }
+  replace_words.keys.each do |ng_word|
+    line.where_is(ng_word).each do |j|
+      ok_word = replace_words[ng_word]
+      puts message("error", "全角「#{ng_word}」の代わりに、半角「#{ok_word}」を使用してください。", i, j)
+      error_count += 1
+    end
+  end
+
+  ["," ,"."].each do |word|
+    line.where_is(word).each do |j|
+      if line[j-1] == " "
+        puts message("error", "「#{word}」の前に不要な半角スペースがあります。", i, j)
+        error_count += 1
+      end
+      if line[j-1] != "\\\\" && line[j+1] != " "
+        puts message("error", "「#{word}」の後には半角スペースが必要です。", i, j)
+        error_count += 1
+      end
+    end
+  end
+
+  line.where_is(")").each do |j|
+    if line[j-1] == " "
+      puts message("error", "「)」の前に不要な半角スペースがあります。", i, j)
+      error_count += 1
+    end
+  end
+
+  line.where_is("(").each do |j|
+    if line[j+1] != " "
+      puts message("error", "「(」の前には半角スペースが必要です。", i, j)
+      error_count += 1
+    end
+  end
+
+  if line[-3] == "," || line[-2] == ","
+    puts message("warn", "「,」で文が終了しています。", i, line.length)
+    warning_count += 1
+  end
+
+  return [warning_count, error_count]
+end
+
+def formula_rule(line, i)
+  return 0 if line[0] != "$"
+  puts message("warn", "文頭が数式です。", i, 0)
+  return 1
+end
+
+def posix_rule(line, i)
+  return 0 if line[-1] == "\n"
+  puts message("error", "ファイルの最後には改行が必要です。", i, line.length)
+  return 1
+end
+
+def message(severity, sentence, i, j)
+  color_prefix = severity == "error" ? "31" : "33"
+  line_number = "#{i+1}".rjust(3, " ")
+  character_number = "#{j+1}".rjust(3, " ")
+  return "#{line_number}:#{character_number}:\e[#{color_prefix}m#{sentence}\e[m"
+end
+
+class String
+  # https://qiita.com/hirokishirai/items/4ef483adba993255ed65
+  def where_is(value) # value が1文字の場合しか正しく動作しない。
+    self.split("").map.with_index{|item, i| i if item == value}.compact!
+  end
+end
+
+file_linter(ARGV[0])
